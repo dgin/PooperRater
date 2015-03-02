254fcfbe310fc41eb3d9d1364596a8edac633a04
Sheldyn Nam-Storm <namstorm@gmail.com>
2015-03-02 15:52:48 -0800

    updates to places model

--- NumberTwoLabs/pooperRater/models.py
+++ NumberTwoLabs/pooperRater/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Places(models.Model):
     floor = models.IntegerField(null=True)
-    unit = models.CharField(null=True, blank=True)
-    address = models.CharField(null=True)
+    unit = models.CharField(null=True, blank=True, max_length=1)
+    address = models.CharField(null=True, max_length=120)
     desc = models.TextField(null=True)
     type = models.IntegerField(null=True)
     start_hours = models.TimeField(null=True)
