c71cfd79a7e4ca24ce09070bc1c897970a9dd467
Daniel Gin <dahgin@gmail.com>
2015-03-11 11:53:49 -0700

    yelp search updated

--- NumberTwoLabs/pooperRater/static/js/yelp_search.js
+++ NumberTwoLabs/pooperRater/static/js/yelp_search.js
@@ -4,4 +4,32 @@ yelp_search = function () {
     console.log(3);
 };
 
-yelp_search();
+function returnUserPosition() {
+    document.getElementById("placeholderWhileGettingLocation").innerHTML = "Now finding where you are. Please enable location services.";
+     getUserPosition()
+    .then(outputPosition)
+    .catch(function(error){
+        console.log("Something went wrong. You may have to enable geolocation services.")
+    });
+
+}
+
+
+//returnUserPosition();
+//getUserPosition()
+//.then(outputPosition)
+//.catch(function(error){
+//        console.log("Something went wrong. You may have to enable geolocation services.")
+//    });
+
+function getUserPosition() {
+    return new Promise(function(resolve, reject) {
+         navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+}
+
+function outputPosition(geoposition) {
+    document.getElementById("placeholderWhileGettingLocation").innerHTML = "";
+    document.getElementById("userLat").innerHTML = "You are at coordinates: " + geoposition.coords.latitude + ", ";
+    document.getElementById("userLong").innerHTML = geoposition.coords.longitude;
+}
