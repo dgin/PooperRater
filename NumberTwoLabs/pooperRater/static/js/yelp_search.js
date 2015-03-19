ff82fb266cf1845f0a516daebb36cb09aac11999
Daniel Gin <dahgin@gmail.com>
2015-03-19 13:59:08 -0700

    fixed some formatting issues in modal form

--- NumberTwoLabs/pooperRater/static/js/yelp_search.js
+++ NumberTwoLabs/pooperRater/static/js/yelp_search.js
@@ -3,10 +3,11 @@ function returnUserPosition() {
     // First disable and hide the location search bar, information
     document.getElementById("ajaxLocation").disabled = true;
     document.getElementById("ajaxLocation").style.display = "none";
+    document.getElementById("ajaxLocation").style.visibility = "hidden";
     document.getElementById("yelpGeoCoordLat").disabled = false;
-    document.getElementById("yelpGeoCoordLat").style.display = "inline-block";
+    //document.getElementById("yelpGeoCoordLat").style.display = "inline-block";
     document.getElementById("yelpGeoCoordLong").disabled = false;
-    document.getElementById("yelpGeoCoordLong").style.display = "inline-block";
+    //document.getElementById("yelpGeoCoordLong").style.display = "inline-block";
     document.getElementById("placeholderWhileGettingLocation").style.display = "block";
 
     // Show placeholder while searching
@@ -23,12 +24,14 @@ function returnUserPosition() {
 function enableLocationSearch() {
     // Enable location search fields and disable geolocation search fields
     document.getElementById("ajaxLocation").disabled = false;
-    document.getElementById("ajaxLocation").style.display = "inline-block";
+    document.getElementById("ajaxLocation").style.display = "block";
     document.getElementById("yelpGeoCoordLat").disabled = true;
-    document.getElementById("yelpGeoCoordLat").style.display = "none";
+    //document.getElementById("yelpGeoCoordLat").style.display = "none";
     document.getElementById("yelpGeoCoordLong").disabled = true;
-    document.getElementById("yelpGeoCoordLong").style.display = "none";
+    document.getElementById("ajaxLocation").style.visibility = "visible";
+    //document.getElementById("yelpGeoCoordLong").style.display = "none";
     document.getElementById("placeholderWhileGettingLocation").style.display = "none";
+    //document.getElementById("placeholderWhileGettingLocation").style.visibility = "hidden";
 }
 
 function getUserPosition() {
